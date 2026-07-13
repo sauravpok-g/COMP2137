d05fed187c8d18fff5ab51f6a9388e1bc376232a
Saurav Pokhrel <Saurav.Pokhrel@MyGeorgian.ca>
2026-07-13 16:36:54 -0400

    Check for installed packages version 1

--- Assignment_2/assignment2.sh
+++ Assignment_2/assignment2.sh
@@ -4,3 +4,41 @@
 # Script: System Configuration
 # Author: Saurav Pokhrel (1863)
 # Desc: Script to determine modifications requried, runs on target system to make modifications and reports on changes made
+
+
+# Varaibles
+
+# Setup Network Interface 
+
+
+# Check Apache2 Status
+
+#apachestatus="$(dpkg-query -W -f='${Status}' apache2 | awk '{print $3}')"
+#quidstatus="$(dpkg-query -W -f='${Status}' squid | awk '{print $3}')"
+
+if dpkg-query -W -f='${Status}' apache2 2>/dev/null | grep -q "install ok installed"; then
+
+    apachestatus="Installed"
+else
+    apachestatus="Not Installed"
+
+fi
+
+if dpkg-query -W -f='${Status}' squid 2>/dev/null | grep -q "install ok installed"; then
+
+    squidstatus="Installed"
+else
+    squidstatus="Not Installed"
+
+fi
+
+cat << EOF
+
+Apache2: $apachestatus
+Squid: $squidstatus
+
+EOF
+
+# Check squid Web proxy status
+
+# Create Users accounts
