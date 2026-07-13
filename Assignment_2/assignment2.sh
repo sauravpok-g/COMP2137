d9856ab35987e4e96514c39422545742f88e0a18
Saurav Pokhrel <Saurav.Pokhrel@MyGeorgian.ca>
2026-07-13 16:03:02 -0400

    Script Boiler Plate COmments

---
+++ Assignment_2/assignment2.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+
+# Script: System Configuration
+# Author: Saurav Pokhrel (1863)
+# Desc: Script to determine modifications requried, runs on target system to make modifications and reports on changes made
