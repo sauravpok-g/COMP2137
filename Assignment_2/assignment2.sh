d1ce3cc40f6a4675255c2619ad5bf3e05da5b8f5
Saurav Pokhrel <Saurav.Pokhrel@MyGeorgian.ca>
2026-07-13 17:53:16 -0400

    Added various checks and workarounds

--- Assignment_2/assignment2.sh
+++ Assignment_2/assignment2.sh
@@ -6,10 +6,36 @@
 # Desc: Script to determine modifications requried, runs on target system to make modifications and reports on changes made
 
 
-# Varaibles
+# Variables
 
-# Setup Network Interface 
+newIP=192.168.16.21/24
+hostname="$(hostname)"
 
+# Find the interface for the IP Specified
+networkintid="192.168.16"
+#networkint="$(ip route | grep $networkintid | awk '{print $3}')"
+
+# Using IP command as route breaks if the checked IP is also the default route
+# -o - one interface per line
+# -4 - only check ipv4
+# Then grep and awk, its always $2 this way and no issues with default route. The IP doesnt show as a network ID but actual IP so skip the last octet
+networkint="$(ip -o -4 addr show | grep 192.168.16. | awk '{print $2}')"
+networkIP="$(ip -o -4 addr show | grep 192.168.16. | awk '{print $4}')"
+
+# Find The Yaml Fiiles that have the IP we are replacing
+# It could be in multiple files, assuming we have to update all
+
+for f in /root/netplan/*.yaml; do
+    if grep -q "$networkIP" "$f"; then
+        sed -i -e "s,${networkIP},${newIP},g" "$f"
+    fi
+done
+
+# Returns the full path to the yaml file(s) that contains the network IP to be replaced
+netplanfilefull="$(grep -l $networkIP /etc/netplan/*.yaml)"
+
+
+# If it found the file then sed it. 
 
 # Check Apache2 Status
 
@@ -17,7 +43,6 @@
 #quidstatus="$(dpkg-query -W -f='${Status}' squid | awk '{print $3}')"
 
 if dpkg-query -W -f='${Status}' apache2 2>/dev/null | grep -q "install ok installed"; then
-
     apachestatus="Installed"
 else
     apachestatus="Not Installed"
@@ -36,6 +61,10 @@ cat << EOF
 
 Apache2: $apachestatus
 Squid: $squidstatus
+
+Network Interface: $networkint
+Network IP: $networkIP
+Netplan File: $netplanfile
 
 EOF
 
